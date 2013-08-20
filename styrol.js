d3019b23fe1238c67dca40cc7a93e7b96ecfb28f
Michał Sałaban <michal@salaban.info>
2013-08-20 16:53:09 +0200

    Initial version, supports select/option dropdowns

---
+++ styrol.js
@@ -0,0 +1,45 @@
+(function ($) {
+
+	$.fn.styrol_select = function () {
+		var select = $(this);
+		var parent = $(select.parent());
+		var options = select.find('option');
+		var ssel = $('<div class="styrol wrapper"></div>');
+		var current = $('<a class="current" href="#">...</a>');
+		var ul = $('<ul class="select"></ul>');
+
+		for (var o=0; o < options.length; o++) {
+			var option = $(options[o]);
+			var li = $('<li class="option"></li>');
+			var a = $('<a href="#" data-value="' + option.attr('value') + '">' + option.text() + '</a>');
+			a.click(function(){
+				var value = $(this).attr('data-value');
+				select.val(value);
+				select.change();
+				current.html($(this).html());
+				ul.slideUp('fast');
+				return false;
+			});
+			li.append(a);
+			ul.append(li);
+		}
+
+		// use the label of the default value
+		var selected_label = ul.find('a[data-value="' + select.val() + '"]').html();
+		if (selected_label) current.html(selected_label);
+
+		current.click(function(){
+			ul.slideToggle('fast');
+		});
+
+		// show
+		ssel.append(current);
+		ssel.append(ul);
+		ul.hide();
+		select.hide();
+		ssel.insertAfter(select);
+
+		return this;
+	}
+
+} (jQuery));
