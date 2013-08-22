ade16eb2c3c6054d8b30b13a8229dfcf481a7ec1
Michał Sałaban <michal@salaban.info>
2013-08-22 17:14:01 +0200

    Add support for checkbox, clean up the code

--- styrol.js
+++ styrol.js
@@ -1,12 +1,10 @@
 (function ($) {
-
-	$.fn.styrol_select = function () {
-		var select = $(this);
+	function _prepare_select(select) {
 		var parent = $(select.parent());
 		var options = select.find('option');
-		var ssel = $('<div class="styrol wrapper"></div>');
+		var ssel = $('<div class="styrol select"></div>');
 		var current = $('<a class="current" href="#">...</a>');
-		var ul = $('<ul class="select"></ul>');
+		var ul = $('<ul></ul>');
 
 		for (var o=0; o < options.length; o++) {
 			var option = $(options[o]);
@@ -37,10 +35,41 @@
 		ssel.append(current);
 		ssel.append(ul);
 		ul.hide();
+		parent.find('.styrol.select').remove();
 		select.hide();
 		ssel.insertAfter(select);
-
-		return this;
 	}
 
+	function _prepare_checkbox(box) {
+		var parent = $(box.parent());
+		var sbox = $('<a class="styrol checkbox" href="#"><span></span></a>');
+		var clickables = sbox;
+
+		if (box.attr('id')) clickables = $.merge(clickables, $('label[for="' + box.attr('id') + '"]'));
+
+		clickables.click(function(){
+			var value = box.prop('checked');
+			box.prop('checked', !value);
+			box.change();
+			if (value) sbox.removeClass('checked');
+			else sbox.addClass('checked');
+			return false;
+		});
+
+		if (box.prop('checked')) sbox.addClass('checked');
+
+		// show
+		parent.find('.styrol.checkbox').remove();
+		box.hide();
+		sbox.insertAfter(box);
+	}
+
+	$.fn.styrol = function () {
+		return this.each(function() {
+			var el = $(this);
+			if (el.is('select')) _prepare_select(el);
+			if (el.is('input[type="checkbox"]')) _prepare_checkbox(el);
+			return this;
+		});
+	}
 } (jQuery));
